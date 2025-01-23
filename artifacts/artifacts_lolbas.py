2e9c66bdc832178868433f2d8707fcca40997c81
John Lukach <john@lukach.net>
2025-01-23 01:12:05 +0000

    lolbas

--- artifacts/artifacts_lolbas.py
+++ artifacts/artifacts_lolbas.py
@@ -1,5 +1,14 @@
 from aws_cdk import (
-    Stack
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_dynamodb as _dynamodb,
+    aws_events as _events,
+    aws_events_targets as _targets,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs,
+    aws_ssm as _ssm
 )
 
 from constructs import Construct
@@ -12,3 +21,104 @@ class ArtifactsLolbas(Stack):
         account = Stack.of(self).account
         region = Stack.of(self).region
 
+    ### LAYERS ###
+
+        extensions = _ssm.StringParameter.from_string_parameter_attributes(
+            self, 'extensions',
+            parameter_name = '/extensions/account'
+        )
+
+        requests = _lambda.LayerVersion.from_layer_version_arn(
+            self, 'requests',
+            layer_version_arn = 'arn:aws:lambda:'+region+':'+extensions.string_value+':layer:requests:7'
+        )
+
+    ### DYNAMODB ###
+
+        lolbas = _dynamodb.Table(
+            self, 'lolbas',
+            table_name = 'lolbas',
+            partition_key = {
+                'name': 'pk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            sort_key = {
+                'name': 'sk',
+                'type': _dynamodb.AttributeType.STRING
+            },
+            billing_mode = _dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy = RemovalPolicy.DESTROY,
+            point_in_time_recovery = True,
+            deletion_protection = False
+        )
+
+    ### IAM ROLE ###
+
+        role = _iam.Role(
+            self, 'role', 
+            assumed_by = _iam.ServicePrincipal(
+                'lambda.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AWSLambdaBasicExecutionRole'
+            )
+        )
+
+        role.add_to_policy(
+            _iam.PolicyStatement(
+                actions = [
+                    'dynamodb:DeleteItem',
+                    'dynamodb:PutItem',
+                    'dynamodb:Query'
+                ],
+                resources = ['*']
+            )
+        )
+
+    ### LAMBDA ###
+
+        lolba = _lambda.Function(
+            self, 'lolba',
+            handler = 'lolbas.handler',
+            runtime = _lambda.Runtime.PYTHON_3_13,
+            code = _lambda.Code.from_asset('lolbas'),
+            architecture = _lambda.Architecture.ARM_64,
+            environment = dict(
+                AWS_ACCOUNT = account,
+                LOLBAS_TABLE = lolbas.table_name
+            ),
+            timeout = Duration.seconds(900),
+            memory_size = 512,
+            retry_attempts = 0,
+            role = role,
+            layers = [
+                requests
+            ]
+        )
+
+        lolbalogs = _logs.LogGroup(
+            self, 'lolbalogs',
+            log_group_name = '/4n6ir/lambda/'+lolba.function_name,
+            retention = _logs.RetentionDays.ONE_MONTH,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+        lolbaevent = _events.Rule(
+            self, 'lolbaevent',
+            schedule = _events.Schedule.cron(
+                minute = '0',
+                hour = '10',
+                month = '*',
+                week_day = 'SUN',
+                year = '*'
+            )
+        )
+
+        lolbaevent.add_target(
+            _targets.LambdaFunction(
+                lolba
+            )
+        )
