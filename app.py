f367c4b3edf5a5c4760e31f5920b69aab13dc8c6
John Lukach <john@lukach.net>
2025-01-27 03:35:20 +0000

    stacks

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ import os
 
 import aws_cdk as cdk
 
+from artifacts.artifacts_blog import ArtifactsBlog
 from artifacts.artifacts_gtfobins import ArtifactsGtfobins
 from artifacts.artifacts_lolbas import ArtifactsLolbas
 from artifacts.artifacts_monitor import ArtifactsMonitor
 from artifacts.artifacts_process import ArtifactsProcess
+from artifacts.artifacts_release import ArtifactsRelease
 from artifacts.artifacts_search import ArtifactsSearch
 from artifacts.artifacts_stack import ArtifactsStack
 
 app = cdk.App()
+
+ArtifactsBlog(
+    app, 'ArtifactsBlog',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
 
 ArtifactsGtfobins(
     app, 'ArtifactsGtfobins',
@@ -47,6 +60,17 @@ ArtifactsMonitor(
 
 ArtifactsProcess(
     app, 'ArtifactsProcess',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-east-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+ArtifactsRelease(
+    app, 'ArtifactsRelease',
     env = cdk.Environment(
         account = os.getenv('CDK_DEFAULT_ACCOUNT'),
         region = 'us-east-2'
---
+++ artifacts/artifacts_release.py
@@ -0,0 +1,20 @@
+from aws_cdk import (
+    Duration,
+    RemovalPolicy,
+    Stack,
+    aws_events as _events,
+    aws_events_targets as _targets,
+    aws_iam as _iam,
+    aws_lambda as _lambda,
+    aws_logs as _logs
+)
+
+from constructs import Construct
+
+class ArtifactsRelease(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        account = Stack.of(self).account
+        region = Stack.of(self).region
