8544afba0e7dbce1dce95f35284071559e37ac90
John Lukach <john@lukach.net>
2025-01-29 02:18:43 +0000

    blog

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # artifacts
+
+- Amazon Linux 2 arm64
+- Amazon Linux 2 ECS Optimized Kernel 5 x86
+- Amazon Linux 2 ECS Optimized x86
+- Amazon Linux 2 Kernel 5 arm64
+- Amazon Linux 2 Kernel 5 x86
+- Amazon Linux 2 x86
+- Amazon Linux 2023 arm64
+- Amazon Linux 2023 ECS Optimized arm64
+- Amazon Linux 2023 ECS Optimized x86
+- Amazon Linux 2023 x86
+- Ubuntu Server 18 LTS arm64
+- Ubuntu Server 18 LTS x86
+- Ubuntu Server 20 LTS arm64
+- Ubuntu Server 20 LTS x86
+- Ubuntu Server 22 LTS arm64
+- Ubuntu Server 22 LTS x86
+- Ubuntu Server 24 LTS arm64
+- Ubuntu Server 24 LTS x86
+- Windows Server 2016 English Core Base x86
+- Windows Server 2016 English Full Base x86
+- Windows Server 2019 English Core Base x86
+- Windows Server 2019 English Full Base x86
+- Windows Server 2022 English Core Base x86
+- Windows Server 2022 English Full Base x86
+- Windows Server 2025 English Core Base x86
+- Windows Server 2025 English Full Base x86
--- blog/blog.py
+++ blog/blog.py
@@ -1,16 +1,67 @@
+import base64
 import boto3
 import datetime
 import json
-import os
 import requests
 
 def handler(event, context):
 
     s3client = boto3.client('s3')
 
-    s3client.download_file('tempmeta  ', 'verification.csv', '/tmp/verification.csv')
+    s3client.download_file('tempmeta', 'verification.csv', '/tmp/verification.csv')
 
+    year = datetime.datetime.now().strftime('%Y')
+    month = datetime.datetime.now().strftime('%m')
+    day = datetime.datetime.now().strftime('%d')
 
+    blogname = year+'-'+month+'-'+day+'-artifacts.md'
+
+    with open('/tmp/'+blogname, 'w') as w:
+        w.write('---\n')
+        w.write('layout: post\n')
+        w.write('title: \"Artifacts - BLAKE3\"\n')
+        w.write('author: \"John Lukach\"\n')
+        w.write('tags: blake3 gtfobins lolbas poppy\n')
+        w.write('---\n\n')
+        w.write('[Artifacts](https://github.com/jblukach/artifacts) release of [BLAKE3](https://github.com/BLAKE3-team/BLAKE3) hashes categorized by operating systems into the content hash, directory, file name, full path, and living-off-the-land sets ([gtfobins](https://gtfobins.github.io) & [lolbas](https://lolbas-project.github.io)) for download of the [poppy](https://github.com/hashlookup/poppy) bloom.\n\n')
+        w.write('| type | total |\n')
+        w.write('|-----|\n')
+        with open('/tmp/verification.csv', 'r') as f:
+            for line in f.readlines():
+                if 'blake3 total' in line or 'blake3 unique' in line or 'b3lols total' in line or 'b3lols unique' in line:
+                    line = line.split(',')
+                    w.write('|'+line[2]+'|'+line[3][:-1]+'|\n')
+        f.close()
+    w.close()
+
+    ssm = boto3.client('ssm')
+
+    token = ssm.get_parameter(
+        Name = '/github/4n6ir/release', 
+        WithDecryption = True
+    )
+
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': 'Bearer '+token['Parameter']['Value'],
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    url = 'https://api.github.com/repos/4n6ir/4n6ir.com/contents/_posts/'+blogname
+
+    with open('/tmp/'+blogname, 'r') as f:
+        content = f.read()
+    f.close()
+
+    content = base64.b64encode(content.encode()).decode()
+
+    data = {
+        'message': blogname,
+        'content': content
+    }
+
+    response = requests.put(url, headers=headers, json=data)
+    print(response.json())
 
     return {
         'statusCode': 200,
