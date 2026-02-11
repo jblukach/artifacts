20ce653ea33c6e1026fce822e07ad051479575fc
John Lukach <hello@lukach.io>
2026-02-11 12:05:59 +0000

    storage

--- artifacts/artifacts_storage.py
+++ artifacts/artifacts_storage.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     Duration,
     RemovalPolicy,
     Stack,
+    aws_iam as _iam,
     aws_s3 as _s3
 )
 
@@ -11,6 +12,38 @@ class ArtifactsStorage(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        role = _iam.Role(
+            self, 'role',
+            role_name = 'artifacts',
+            assumed_by = _iam.ServicePrincipal(
+                'ec2.amazonaws.com'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'EC2InstanceProfileForImageBuilder'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'EC2InstanceProfileForImageBuilderECRContainerBuilds'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'AmazonS3FullAccess'
+            )
+        )
+
+        role.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'AmazonSSMManagedInstanceCore'
+            )
+        )
 
         matchmetadata = _s3.Bucket(
             self, 'matchmetadata',
@@ -37,6 +70,17 @@ class ArtifactsStorage(Stack):
         matchmetaoutput.add_lifecycle_rule(
             expiration = Duration.days(1),
             noncurrent_version_expiration = Duration.days(1)
+        )
+
+        matchmetastaged = _s3.Bucket(
+            self, 'matchmetastaged',
+            bucket_name = 'matchmetastaged',
+            encryption = _s3.BucketEncryption.S3_MANAGED,
+            block_public_access = _s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy = RemovalPolicy.DESTROY,
+            auto_delete_objects = True,
+            enforce_ssl = True,
+            versioned = False
         )
 
         matchmetatemporary = _s3.Bucket(
